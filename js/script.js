a78eb3394d8bda213892ab05c7627c9fc8232fd7
mannuccimattia <mannuccimattia@gmail.com>
2025-04-03 15:55:56 +0200

    [FIX] Fixed async: added counter

--- js/script.js
+++ js/script.js
@@ -7,15 +7,17 @@ const mailList = document.getElementById("mail-list");
 let mailArray = [];
 // definisco il numero di mail da generare
 const mailNumber = 10;
+// contatore
+let counter = 0;
+
 
 
 for (let i = 0; i < mailNumber; i++) {
   axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then(res => {
-    if (i < mailNumber - 1) {
-      mailArray.push(res.data.response);
-    }
-    else if (i == mailNumber - 1) {
-      mailArray.push(res.data.response);
+    mailArray[i] = res.data.response;
+    counter++;
+
+    if (counter === mailNumber) {
       console.log("mailArray è", mailArray);
 
       for(let j=0; j<mailNumber; j++){
@@ -24,12 +26,34 @@ for (let i = 0; i < mailNumber; i++) {
         listItem.appendChild(itemContent);
         mailList.appendChild(listItem);
       }
-      // const listItemsArray = mailArray.map(mail => `<li>${mail}</li>`);
-      // console.log("listItemsArray è", listItemsArray);
-
-      // for(let j=0; j<mailNumber; j++){
-      //   mailList.innerText = listItemsArray[j];
-      // }
+     
     }
   });
 }
+
+
+
+
+// SOLUZIONE PROMISE.ALL
+// let promises = [];
+
+// for(let i = 0; i < mailNumber; i++){
+//   promises.push(axios.get("https://flynn.boolean.careers/exercises/api/random/mail"));
+// }
+// console.log(promises)
+// Promise.all(promises)
+//   .then(responses => {
+//     responses.forEach(response => {
+//       mailArray.push(response.data.response);
+//     });
+//     console.log("mailArray è", mailArray);
+
+//     mailArray.forEach(email => {
+//       const listItem = document.createElement("li");
+//       listItem.textContent = email;
+//       mailList.appendChild(listItem);
+//     });
+//   })
+//   .catch(error => {
+//     console.error("Errore nel recupero delle mail:", error);
+//   });
